04949a9b7142a8ff91c67931d80b7f129005d3e4
Daniel <daniel@ai-asylum.com>
2015-01-02 22:59:57 +0000

    Wrote test and function 'calcAnswer' that will send the numbers to the chosen shapes volume function.

--- src/app/volumeCalc.js
+++ src/app/volumeCalc.js
@@ -1,24 +1,24 @@
-function cylinderVolume (r,h) {
+function cylinderVolume (r, h) {
 	if (isNumeric(r) && isNumeric(h)) {
-		return Math.PI * Math.pow(r,2) * h;
+		return Math.round(Math.PI * Math.pow(r,2) * h*100)/100;
 	}
 	else {
 		return 'Please insert number';
 	}
 }
 
-function coneVolume (r,h) {
+function coneVolume (r, h) {
 	if (isNumeric(r) && isNumeric(h)) {
-		return cylinderVolume(r,h)/3;
+		return Math.round(cylinderVolume(r,h)/3*100)/100;
 	}
 	else {
 		return 'Please insert number';
 	}	
 }
 
-function sphereVolume (r,h) {
+function sphereVolume (r, h) {
 	if (isNumeric(r) && isNumeric(h)) {
-		return (Math.PI * Math.pow(r,3)) * (4/3);
+		return Math.round((Math.PI * Math.pow(r,3)) * (4/3) * 100)/100;
 	}
 	else {
 		return 'Please insert number';
@@ -27,4 +27,17 @@ function sphereVolume (r,h) {
 
 function isNumeric(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function calcAnswer(shape, r, h) {
+	switch (shape) {
+		case 'cylinder':
+			return cylinderVolume(r, h);
+		case 'cone':
+			return coneVolume(r, h);
+		case 'sphere':
+			return sphereVolume(r, h);
+		default:
+			return 'Please pick a shape';
+	}
 }
--- src/app/volumeCalc.spec.js
+++ src/app/volumeCalc.spec.js
@@ -1,7 +1,7 @@
 describe('cylinderVolume', function () {
 	it('can give a right answer', function () {
 		answer = cylinderVolume(3,6);
-		expect(Math.round(answer * 100) / 100).toBe(169.65);
+		expect(answer).toBe(169.65);
 	});
 	it('should thrown a meaningful error on invalid input', function () {
 		answer = cylinderVolume('fish',6);
@@ -12,7 +12,7 @@ describe('cylinderVolume', function () {
 describe('coneVolume', function () {
 	it('can give a right answer', function () {
 		answer = coneVolume(3,6);
-		expect(Math.round(answer * 100) / 100).toBe(56.55);
+		expect(answer).toBe(56.55);
 	});
 	it('should thrown a meaningful error on invalid input', function () {
 		answer = coneVolume('fish',6);
@@ -23,7 +23,7 @@ describe('coneVolume', function () {
 describe('sphereVolume', function () {
 	it('can give a right answer', function () {
 		answer = sphereVolume(3,6);
-		expect(Math.round(answer * 100) / 100).toBe(113.1);
+		expect(answer).toBe(113.1);
 	});
 	it('should thrown a meaningful error on invalid input', function () {
 		answer = sphereVolume('fish',6);
@@ -39,11 +39,22 @@ describe('isNumeric', function () {
 		expect(isNumeric('5')).toBe(true);
 	});
 	it('returns false on non-numbers', function () {
-		answer = coneVolume(3,6);
 		expect(isNumeric('limburger')).toBe(false);
 	});
 	it('returns false on mixed strings', function () {
-		answer = coneVolume(3,6);
 		expect(isNumeric('limburger42')).toBe(false);
 	});	
+});
+
+describe('calcAnswer', function () {
+	it('calls the correct method given the shape', function () {
+		answer = calcAnswer('cylinder', 3, 6);
+		expect(answer).toBe(169.65);
+		
+		answer = calcAnswer('cone', 3, 6);
+		expect(answer).toBe(56.55);	
+
+		answer = calcAnswer('sphere', 3, 6);
+		expect(answer).toBe(113.1);		
+	});
 });
